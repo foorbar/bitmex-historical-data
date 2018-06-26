10ca55254494f17ccafaf7276271326be042d9a9
Zolika <zh4917@ic.ac.uk>
2018-06-26 13:48:26 +0100

    Finished the program for reading historical data from the bitmex api

--- bitmex_main.py
+++ bitmex_main.py
@@ -1,11 +1,8 @@
-from bitmex_requests import writeLargeJson;
+from bitmex_requests import writeJson;
 from datetime import datetime
 
-start = datetime(2017, 1, 1, 0, 0);
-end = datetime(2017, 1, 1, 0, 0)
+start = datetime(2018, 3, 30, 0, 0);
+end = datetime(2018, 6, 26, 0, 0)
 cols = ["open", "close", "high", "low"];
 
-writeLargeJson("XBTUSD", start, end, "1m", cols,"btc.txt");
-writeLargeJson(".ETHXBT", start, end, "1m", cols,"eth.txt");
-writeLargeJson(".LTCXBT", start, end, "1m", cols,"ltc.txt");
-writeLargeJson(".XRPXBT", start, end, "1m", cols,"xrp.txt");
+writeJson("ETHM18", start, end, "1m", cols,"eth_minutes.txt");
--- bitmex_requests.py
+++ bitmex_requests.py
@@ -1,7 +1,10 @@
 import requests;
 import json;
+import time;
 from datetime import datetime
 from datetime import timedelta
+
+requestNum = 0;
 
 #generates a key-value pair in string format
 def generateFilter(currency):
@@ -17,8 +20,11 @@ def getJsonChunk(currency, startTime, endTime, interval, columns):
     end = endTime;
     parameters = {"binSize": interval, "columns": columns, "filter": filter, "startTime": start, "endTime": end};
     response = requests.get("https://www.bitmex.com/api/v1/trade/bucketed", params = parameters);
+    global requestNum;
+    requestNum += 1;
     data = response.json();
     print(response.status_code);
+    print (requestNum);
     return data;
 
 #writes the output json out to the output file
@@ -26,10 +32,19 @@ def writeOut(output, file):
     with open(file, 'a') as outfile:
         json.dump(output, outfile);
 
-#returns a datetime object for the next hour
-def getNextHour(time):
-    nextHour = time + timedelta(hours = 1);
-    return nextHour;
+#returns a datetime object 480 minutes later
+def jump(time, interval):
+    if interval == '1m':
+        nextTime = time + timedelta(minutes = 480);
+
+    if interval == '1h':
+        nextTime = time + timedelta(hours = 480);
+
+    if interval == '1d':
+        nextTime = time + timedelta(days = 480);
+
+    return nextTime;
+
 
 #returns a string from the datetime object
 def getTimeString(time):
@@ -37,17 +52,24 @@ def getTimeString(time):
     return timeString;
 
 #supports requesting data that exceed limit of entries per request
-def writeLargeJson(currency, startTime, endTime, interval, columns, file):
+def writeJson(currency, startTime, endTime, interval, columns, file):
     #creates an empty output file
     open(file, "w").close();
 
     currentTime = startTime;
     output = [];
 
-    #makes api requests in hourly chunks until the endTime is reached
-    while currentTime != endTime:
-        nextHour = getNextHour(currentTime);
+    #makes api requests in 480 minutes chunks until the endTime is reached
+    while currentTime < endTime:
+        nextTime = jump(currentTime, interval);
         print(getTimeString(currentTime));
-        output += getJsonChunk(currency, getTimeString(currentTime), getTimeString(nextHour), interval, columns);
-        currentTime = getNextHour(currentTime);
+        output += getJsonChunk(currency, getTimeString(currentTime), getTimeString(nextTime), interval, columns);
+        currentTime = jump(currentTime, interval);
+
+        if requestNum > 120:
+            print("reached maximum amount of requests");
+            time.sleep(320);
+            global requestNum;
+            requestNum = 0;
+
     writeOut(output, file);
